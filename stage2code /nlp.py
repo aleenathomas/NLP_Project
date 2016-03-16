24620ce6196b73a8fec99edfc7e02b48cfc86ee8
aleenathomas <aleenathomas203@gmail.com>
2016-03-16 19:48:17 +0530

    added stages

--- stage2code /nlp.py
+++ stage2code /nlp.py
@@ -50,27 +50,4 @@ rem_file.seek(0,0)
 for line in lines:
 	rem_file.write(line)
 
-
-
-
-
-#additional-make a list of words from the file
-rem_file.seek(0,0)
-contents = rem_file.read()
-words = contents.split()
-
-#count the occurences of each distinct word in the file
-count = collections.Counter()
-for word in words:
-	count[word] = count[word] + 1
-
-#write into vocabulary file those words that have count>=2
-vocab = open("vocabulary.txt","w")
-for word in count:
-	if count[word] >= 2:
-		vocab.write(word+"\n")
-	
-
-
-vocab.close()
 rem_file.close()

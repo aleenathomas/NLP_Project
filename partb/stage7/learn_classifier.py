b8dce47d1cad423f05ee2a2915a1a55080b68741
reshmat <reshmathomas1194@gmail.com>
2016-04-10 11:21:35 +0530

    Modified learn_classifier.py to make it work for bigrams

---
+++ partb/stage7/learn_classifier.py
@@ -0,0 +1,126 @@
+#!/usr/bin/env python
+#training the classifier
+
+import collections
+import pickle
+import math
+import re
+
+vocabulary="../stage6/vocabulary.txt"
+removed="../stage6/removed"
+
+#function to count the number of words in a file(vocabulary)
+def countwords(filename):
+	f = open(filename, "r+")
+	contents=f.read()
+	words=contents.split()
+	
+	count = 0
+	for word in words:
+        	count += 1
+        f.close()
+    	return count
+    	
+    	
+#function to count the number of reviews
+def countnumofreviews(filename):
+	number_of_reviews = 0
+	with open(filename) as datafile:
+   		for line in datafile:
+        		if line.strip():
+        			number_of_reviews += 1
+        	
+        return number_of_reviews
+        
+#to find the number of reviews in each class
+def classnum(filename,classname):
+	num = 0
+	with open(filename) as datafile:
+   		for line in datafile:
+  
+   			if line[-2:-1] == classname:
+   				num = num + 1
+   				
+   	return num
+
+
+   	
+#find the count of a particular word in a class
+def wordcount(filename,word,classname,bigram):
+	wordnum = 0
+	with open(filename) as datafile:
+   		for line in datafile:
+  
+   			if line[-2:-1] == classname:
+				if(bigram == False):
+	   				words = line.split()
+				else:
+					#create bigrams from the line
+					words = collections.Counter(zip(line, islice(line,1,None)))
+					
+	   			for w in words:
+	   				if w == word:
+	   					wordnum += 1 
+				
+	return wordnum  
+   	
+#to find prior probabilities
+def class_prior(filename,classname):
+	number_of_reviews = countnumofreviews(filename)
+	num = classnum(filename,classname)	
+	prior = (float)(num)/number_of_reviews
+	return prior
+
+#get the first word of the bigram
+def getfirstword(bigram):
+	return bigram.split(' ', 1)[0]	
+
+
+				
+#to find conditional probabilities
+def class_cond(filename,bigram,classname):
+	voc_num=countwords(vocabulary)
+	
+	num = wordcount(filename,bigram,classname,True)
+	#wordcountclass function is not used here.
+	word = getfirstword(bigram)
+	den = wordcount(filename,word,classname,False)
+	cond_prob = float(num + 1)/ ( den + voc_num )
+	return cond_prob
+		    	
+
+def learn(filename,vocfilename):
+	#make a list of all words and bigrams in the vocabulary
+	f=open(vocfilename,"r")
+	contents=f.read()
+	words = contents.split('\n')
+	f.close()
+
+
+	#keyword is a dict indexed by each word in the vocabulary
+	keyword = collections.Counter()
+
+	#store the conditionalprob in dictionary 
+	#it stores a list of 2 elements: log(P(word|+)) and log(P(word|-))
+	
+	for word in words:
+		#checking whether the word is a bigram
+		bigram = False
+		count = len(re.findall(r'\w+', word))
+		if(count==2):
+			bigram = True
+		if(bigram == True):
+			pos_class_prob_log = math.log(class_cond(filename,word,'+'),2)
+			neg_class_prob_log = math.log(class_cond(filename,word,'-'),2)
+			keyword[word] = [pos_class_prob_log,neg_class_prob_log]
+
+	#save the dict of learned probabilities in the file classifier.pickle
+
+	with open('classifier.pickle', 'wb') as handle:
+		pickle.dump(keyword, handle)
+	f.close()
+	
+learn(removed,vocabulary)
+#changed wordcount function,class_cond and learn function to make the algo work for bigrams
+#used the formula P(sentence|class) = product of P(word-1,word | class) = count(word-1,word in class)+1 / count(word-1 in class) + size of vocabulary
+#removed wordcountclass function
---
+++ partb/stage7/stage7code.py
@@ -0,0 +1,128 @@
+import learn_classifier
+import code_to_remove_stop_words
+import code_to_build_vocabulary
+
+# divide the set into 10
+# using classnum, find the no. of reviews in pos and neg
+
+# function to find the actual class of a given test sentence
+def findclass(line):
+	if line[-2:-1] == "+":
+		return 1
+	else:
+		return 0
+
+def findclasscond(line,category,filename):
+	words=line.split()
+	prob=1
+	for word in words:
+		prob*= learn_classifier.class_cond(filename,word,category)
+	return prob
+
+def findposterior(line,category,filename):
+	classconditionalprob = findclasscond(line,category, filename)
+	posterior = classconditionalprob * learn_classifier.class_prior(filename,category)
+	return posterior		
+			
+filename = "../stage2/removed"
+postotal = learn_classifier.classnum(filename, "+")
+negtotal = learn_classifier.classnum(filename, "-")
+
+datafile = open(filename,'r')
+lines = datafile.readlines()
+
+pbreak=0
+nbreak=0
+final_acc = 0
+for k in range(10):
+        #alg to test mixed data set
+
+	testset = open("test.txt","w+")
+	trainset = open("train.txt","w+")
+        if pbreak != 0:
+		for index in range(0, pbreak+1):
+			if findclass(lines[index]) == 1:
+				trainset.write(lines[index])
+	i = 0	
+	pj = pbreak
+	while i < postotal/10:
+		while (pj<postotal+negtotal):
+			if findclass(lines[pj]) == 1:
+				testset.write(lines[pj])
+				pj = pj+1
+				break
+			pj = pj+1
+		i = i+1
+	pbreak = pj
+	for index in range(pbreak + 1, postotal+negtotal):
+		if findclass(lines[index]) == 1:
+			trainset.write(lines[index])
+	#positives added
+	if nbreak != 0:
+		for index in range(0, nbreak+1):
+			if findclass(lines[index]) == 0:
+				trainset.write(lines[index])
+	i = 0
+	nj = nbreak
+	while i < negtotal/10:
+		while (nj<postotal+negtotal):
+			if findclass(lines[nj]) == 0:
+				testset.write(lines[nj])
+				nj = nj+1
+				break
+			nj = nj+1
+		i = i+1
+	nbreak = nj
+	for index in range(nbreak + 1, postotal+negtotal):
+		if findclass(lines[index]) == 0:
+			trainset.write(lines[index])
+	#negatives added
+	##end
+
+	testset.close()	
+
+	# code to train "train.txt"
+	code_to_remove_stop_words.remove_stopwords("train.txt")
+	code_to_build_vocabulary.build_vocab("removed")
+	#call create_bigrams
+	learn_classifier.learn("removed", "vocabulary.txt")
+
+	# code to test the sentences in "test.txt"
+	tp = 0 
+	tn = 0
+	fp = 0
+	fn = 0
+
+	#contents = testset.read()
+	#lines = contents.split('\n')
+
+	#print len(lines)
+
+	with open("test.txt") as f:
+	    content = f.readlines()
+
+	for line in content:	
+		post_pos = findposterior(line, "+", "removed")	#find the posterior prob of positive 
+		post_neg = findposterior(line, "-", "removed")	#find the posterior prob of negative
+		if post_pos > post_neg:			#predict
+			prediction = 1
+		else: 
+			prediction = 0
+		actual = findclass(line)	
+		if  actual == 1 and prediction == 1:
+			tp += 1	
+		elif  actual == 0 and prediction == 0:
+			tn += 1
+		elif  actual == 0 and prediction == 1:
+			fp += 1	
+		else: 
+			fn +=1	
+		#print (str(actual)+" " +str(prediction))
+					
+	accuracy = float(tp + tn)/float(tp + tn + fp + fn)		
+	print accuracy	
+	final_acc = final_acc + accuracy
+
+	trainset.close()
+	f.close()	
+print "final accuracy = " + str(final_acc/10)
